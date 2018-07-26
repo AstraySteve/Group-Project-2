170f83c8c5efb17b373df80e12b13252bd36f905
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-07-26 19:21:37 -0400

    moved sql files to folder db, init user and teams api-routes, init handlebars page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,8 +13,17 @@ module.exports = function(app) {
   });
 
   //TODO: add links to other pages
+  app.get("/login", function(req,res) {
+    //TODO: link to login page
+  });
 
+  app.get("/profile/:userid", function(req,res) {
+    //TODO: link to user profile page
+  });
+
+  //NOTES: might need create user page, create team page, compare team page
   
+  //TEST CODE REMOVE WHEN DONE
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
@@ -24,6 +33,7 @@ module.exports = function(app) {
     });
   });
 
+  //CATCH ALL CODE
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
