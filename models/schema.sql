64d3eac81a5e47aadcd1cfdd6e7608e36a15c06c
Unknown <home9800@hotmail.com>
2018-08-07 20:50:50 -0400

    Fixed Header

--- models/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
-
-DROP DATABASE IF EXISTS project_hockey;
-CREATE DATABASE project_hockey;
